1da2a697fef7aca1600a7e21684256ab576cd181
ForhadRobe <i.rforhad@gmail.com>
2022-08-15 13:44:49 +0600

    first commit

---
+++ a.js
@@ -0,0 +1,23 @@
+/* function compare(a, b) {
+    if (a == b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+const result = compare(12, '12');
+console.log(result); */
+
+/* function compare(a, b) {
+    if (a.toString() === b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+const result = compare(25, 25);
+console.log(result);
+console.log("123" + 123); */
+let a = 'hi'
+let b = 'there'
+console.log(a + b)
